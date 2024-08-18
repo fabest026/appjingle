de1b436f85998d6bacd32f75073eab7ef5f5d9e9
Farhan Akbar <rasolehri@gmail.com>
2024-08-18 17:41:15 +0500

    bb

---
+++ .history/pages/15_⏺️_image_20240818173922.py
@@ -0,0 +1,50 @@
+## loading all the environment variables
+from dotenv import load_dotenv
+load_dotenv() 
+
+# Import Important libraries
+import streamlit as st
+
+import torch as t
+from diffusers import FluxPipeline
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+
+
+# sidebar for the user input
+
+with st.sidebar:
+    st.markdown(
+        "<style>h1 {text-align: center;}</style>",
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        "<style>h1 {text-align: center; color: black;}</style>",
+        unsafe_allow_html=True
+    )
+    st.title("Input Settings")
+    
+    st.markdown(
+        "<style>"
+        "h4 {text-align: left; color: black; margin-top: 4px;}"
+        "p {text-align: left; color: black;}"
+        "</style>",
+        unsafe_allow_html=True
+    )
+    st.markdown("<h4>Enter Details for the Rephrasing: </h4>", unsafe_allow_html=True)
+
+    # Blog Title
+    input = st.text_input("Text/Paragraph") 
+    
+
+prompt = "{input}"
+image = pipe(
+    prompt,
+    guidance_scale=0.0,
+    num_inference_steps=4,
+    max_sequence_length=256,
+    generator=torch.Generator("cpu").manual_seed(0)
+).images[0]
+image.save("flux-schnell.png")
+
