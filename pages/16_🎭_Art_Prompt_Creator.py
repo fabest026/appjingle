b5070877abefeea839c6748d4fce2bd5a9b8f385
Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2025-03-19 02:51:39 +0500

    Update 16_🎭_Art_Prompt_Creator.py

--- pages/16_🎭_Art_Prompt_Creator.py
+++ pages/16_🎭_Art_Prompt_Creator.py
@@ -10,7 +10,7 @@ import random
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # function to load Gemini Pro model and get reponses
-model=genai.GenerativeModel("gemini-1.5-pro-latest")
+model=genai.GenerativeModel("gemini-2.0-flash")
 def get_gemini_response(input,image):
     if input!="":
         response=model.generate_content([input,image])
