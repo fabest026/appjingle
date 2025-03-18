39c547c4e877620eefdda0bfdbe2b629aefaa56a
Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2025-03-19 02:49:52 +0500

    Update 10_🧙‍♂️_Ai_Detection_Remover.py

--- pages/10_🧙‍♂️_Ai_Detection_Remover.py
+++ pages/10_🧙‍♂️_Ai_Detection_Remover.py
@@ -14,7 +14,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 generation_config = {
   "temperature": 1,
   "top_p": 0.95,
-  "top_k": 64,
+  "top_k": 40,
   "max_output_tokens": 8192
 }
 
@@ -39,7 +39,7 @@ safety_settings = [
 
 # Load Gemini Pro model
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash",
+  model_name="gemini-2.0-flash",
   generation_config=generation_config,
   # safety_settings = Adjust safety settings
   # See https://ai.google.dev/gemini-api/docs/safety-settings
